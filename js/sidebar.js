a5a822e7dc97eebdce03944b6b3f7c800f133d4c
DallOner <paspau2@hotmail.com>
2018-11-08 17:41:09 -0500

    Added Overlay effect when open boards menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,9 @@
 function sideOpen() {
     document.getElementById("sidebarMenu").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
 function sideClose() {
     document.getElementById("sidebarMenu").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
